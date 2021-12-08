b7d095c6fb6ceffefac30be0aa218506b3a2ff0d
Salvydas Lukosius <sall@w-ss.io>
2021-12-08 21:21:21 +0000

    modified:   history-search-multi-word.plugin.zsh

--- history-search-multi-word.plugin.zsh
+++ history-search-multi-word.plugin.zsh
@@ -3,22 +3,22 @@
 #
 
 # According to the standard:
-# http://z-shell.github.io/ZSH-TOP-100/Zsh-Plugin-Standard.html
+# https://github.com/z-shell/zi/wiki/Zsh-Plugin-Standard
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
 HSMW_REPO_DIR="${0:h}"
 
-if [[ ${zsh_loaded_plugins[-1]} != */history-search-multi-word && -z ${fpath[(r)${0:h}]} ]]
-then
-  fpath+=( "${0:h}" )
-fi
+if [[ $PMSPEC != *f* ]] {
+  fpath+=( "${0:h}/functions" )
+}
 
 autoload history-search-multi-word hsmw-context-main
 zle -N history-search-multi-word
 zle -N history-search-multi-word-backwards history-search-multi-word
 zle -N history-search-multi-word-pbackwards history-search-multi-word
 zle -N history-search-multi-word-pforwards history-search-multi-word
+
 [[ ${+termcap[Co]} = 1 && ${termcap[Co]} = 256 ]] && {
   zstyle -s ":history-search-multi-word" highlight-color tmp || zstyle ":history-search-multi-word" highlight-color "bg=17"
   typeset -gA HSMW_HIGHLIGHT_STYLES
