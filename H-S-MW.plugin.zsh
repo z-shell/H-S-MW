f833b06817f4c948e78f53e0f1059f14834c0c6d
Salvydas Lukosius <sall@w-ss.io>
2022-10-30 14:50:52 +0000

    Update H-S-MW.plugin.zsh
    Signed-off-by: Salvydas Lukosius <sall@w-ss.io>

--- H-S-MW.plugin.zsh
+++ H-S-MW.plugin.zsh
@@ -9,14 +9,14 @@
 #   source {where-unpacked}/H-S-MW.plugin.zsh to ~/.zshrc.
 #
 # Standardized $0 Handling
-# https://z.digitalclouds.dev/community/zsh_plugin_standard#zero-handling
+# https://wiki.zshell.dev/community/zsh_plugin_standard#zero-handling
 0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
 HSMW_REPO_DIR="${0:h}"
 
 # Functions directory
-# https://z.digitalclouds.dev/community/zsh_plugin_standard#funtions-directory
+# https://wiki.zshell.dev/community/zsh_plugin_standard#funtions-directory
 if [[ $PMSPEC != *f* ]] {
   fpath+=( "${0:h}/functions" )
 }
